dbf17908cd4f954be0c5a5d78c02c903b84116b1
schwarro <rob.ap.schwartz@gmail.com>
2018-08-02 16:52:26 -0400

    revised code

--- crm.rb
+++ crm.rb
@@ -15,9 +15,8 @@ get '/contacts' do
 end
 
 get '/contacts/:id' do
-  # params[:id] contains the id from the URL
+  # params [:id] contains the id from teh URL
   @contact = Contact.find_by({id: params[:id].to_i})
-
   if @contact
     erb :show_contact
   else
