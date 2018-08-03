91a752c0cc8a5b5023e32a30da6a49bb022924ca
schwarro <rob.ap.schwartz@gmail.com>
2018-08-03 15:38:41 -0400

    completed exercise today

--- crm.rb
+++ crm.rb
@@ -14,6 +14,10 @@ get '/contacts' do
   erb :contacts
 end
 
+get '/contacts/new' do
+  erb :new
+end
+
 get '/contacts/:id' do
   # params [:id] contains the id from teh URL
   @contact = Contact.find_by({id: params[:id].to_i})
@@ -22,6 +26,51 @@ get '/contacts/:id' do
   else
     raise Sinatra::NotFound
   end
+end
+
+get '/contacts/:id/edit' do
+  @contact = COntact.find_by(id: params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+post '/contacts' do
+  Contact.create(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+  )
+  redirect to('/contacts')
+end
+
+put '/contacts/:id' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    @contact.update(
+      first_name: params[:first_name]
+      last_name:  params[:last_name]
+      email:      params[:email],
+      note:       params[:note]
+    )
+
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+delete '/contacts/:id' do
+  @contact = Contact.find_by(params[:id].to_i)
+  if @contact
+    @contact.delete
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end 
 end
 
 get '/about' do
